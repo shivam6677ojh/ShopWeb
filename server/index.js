260d9514006561e0b080a18c29b296f50df19948
shivam ojha <143830066+shivam6677ojh@users.noreply.github.com>
2026-02-19 11:11:27 +0530

    fix: correct PORT assignment to prioritize environment variable

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(helmet({
     crossOriginResourcePolicy : false
 }))
 
-const PORT = 8080 || process.env.PORT 
+const PORT = process.env.PORT 
 
 app.get("/",(request,response)=>{
     ///server to client
